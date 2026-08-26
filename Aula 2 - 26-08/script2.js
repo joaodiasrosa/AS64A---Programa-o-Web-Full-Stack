745ec1bf8ffc4b8b5841c364fe08b2f633598e10
joaodiasrosa <joaodiasrosa08@gmail.com>
2026-08-26 20:54:32 -0300

    Aula 2

--- Aula 2 - 26-08/script2.js
+++ Aula 2 - 26-08/script2.js
@@ -6,3 +6,18 @@ function displayRadioValue(){
             document.getElementById("result").innerHTML ="Genero: "+elemento[i].value;
     }
 }
+
+function getOption(){
+    selectElement = document.querySelector('#select1');
+    output = selectElement.value;
+    document.querySelector('.output').textContent = output;
+}
+
+function getCheckedBox(){
+    let checkBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    let text = "";
+    for (i=0;i<checkBoxes.length; i++) {
+        text = '${text}, ${checkBoxes[i].value}';       
+    }
+    document.getElementById('pri').innerHTML = text;
+}
